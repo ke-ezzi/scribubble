a9479dacd308e2c4f4182078f73e723fa78fee35
yeji-l <lyj990701@naver.com>
2022-12-03 19:11:35 +0900

    fix: front

--- src/DirtyFigmaExport.js
+++ src/DirtyFigmaExport.js
@@ -1,3 +1,5 @@
+import { Center } from "@react-three/drei"
+
 export function Underlay() {
   return (
     <div
@@ -14,10 +16,10 @@ export function Underlay() {
         justifyContent: "flex-start",
         pointerEvents: "none",
       }}>
-      <div style={{ width: "100%", padding: 0, display: "inline-flex", flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+      <div style={{ width: "100%", padding: 8, display: "inline-flex", flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
         <p
           style={{
-            fontFamily: "'Antonio', sans-serif",
+            fontFamily: "'love",
             flex: "1 1 0%",
             height: 30,
             fontSize: 30,
@@ -28,85 +30,13 @@ export function Underlay() {
           }}>
           Scribubble
         </p>
-        <div style={{ flex: "1 1 0%", display: "flex", gap: "2em" }}></div>
-        <p style={{ flex: "1 1 0%", height: 30, fontSize: 30, lineHeight: "30px", textAlign: "right", color: "black" }}>⎑</p>
-      </div>
-      <div style={{ height: 60 }} />
-      <div style={{ width: "100%", padding: 0, display: "inline-flex", flexDirection: "row", alignItems: "flex-start", justifyContent: "center" }}>
-        <p style={{ flex: "1 1 0%", height: "100%", fontSize: 12, lineHeight: "1.5em", color: "black" }}>
-          <b>Stones, Metals and Gems</b>
-          <br />
-          A Universal Deity
-          <br />
-          <b>—</b>
-        </p>
-        <div style={{ width: 10 }} />
-        <p
-          style={{
-            transform: "rotate3d(0, 0, 1, 90deg) translate3d(100%,10px,0)",
-            transformOrigin: "right",
-            fontSize: 12,
-            fontWeight: "700",
-            lineHeight: "100%",
-            textAlign: "right",
-            color: "black",
-            whiteSpace: "nowrap",
-          }}>
-          DRAG POINTER &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ●
-        </p>
-      </div>
-      <div style={{ height: 10 }} />
-      <div
-        className="full"
-        style={{
-          fontFamily: "'Antonio', sans-serif",
-          width: "100%",
-          flex: "1 1 0%",
-          padding: 0,
-          display: "inline-flex",
-          flexDirection: "row",
-          alignItems: "flex-end",
-          justifyContent: "center",
-        }}>
-        <p style={{ flex: "1 1 0%", fontSize: 240, lineHeight: "1em", color: "black", margin: 0, letterSpacing: -20 }}>Bubble</p>
-        <div style={{ width: 10 }} />
-        <p style={{ flex: "1 1 0%", fontSize: 240, lineHeight: "100%", textAlign: "right", color: "black", margin: 0, letterSpacing: -10 }}>01-</p>
-      </div>
-      <div style={{ height: 60 }} />
-      <div
-        style={{
-          pointerEvents: "all",
-          pointer: "auto",
-          width: "100%",
-          padding: 0,
-          display: "inline-flex",
-          flexDirection: "row",
-          alignItems: "flex-end",
-          justifyContent: "center",
-        }}>
-        <p className="full" style={{ whiteSpace: "nowrap", flex: "1 1 0%", fontSize: 12, lineHeight: "1.5em", color: "black" }}>
-          <b>Wonders of Antiquity</b>
-          <br />
-          Pythagorean Mathematics
-        </p>
-        <div style={{ width: 10 }} />
-        <p
-          className="full"
-          style={{
-            fontFamily: "'Antonio', sans-serif",
-            flex: "1 1 0%",
-            fontSize: 20,
-            fontWeight: "700",
-            lineHeight: "1em",
-            textAlign: "center",
-            color: "black",
-            letterSpacing: -0.5,
-            whiteSpace: "nowrap",
-          }}>
-          Make your page
-        </p>
-        <div style={{ width: 10 }} />
-        <p className="full" style={{ flex: "1 1 0%", fontSize: 12, lineHeight: "1em", textAlign: "right", color: "black" }}></p>
+        <div style={{ flex: "1 1 0", display: "flex", gap: "6em" }}>
+        <p>Work</p>
+        <p>Event</p>
+        <p>Page</p>
+        <p>About</p>
+        <p>Start Scribubble</p>
+        </div>
       </div>
     </div>
   )
@@ -114,9 +44,23 @@ export function Underlay() {
 
 export function Overlay() {
   return (
-    <div style={{ position: "absolute", bottom: 40, right: 40 }}>
-      <p style={{ flex: "1 1 0%", fontSize: 12, lineHeight: "1em", textAlign: "right", color: "black" }}>
-        <a href="http://pmnd.rs/">pmnd.rs</a> <a href="https://github.com/pmndrs">git</a> <a href="https://codesandbox.io/s/zxpv7">csb</a>
+    <div style={{ position: "absolute",left: 300, bottom: 300, textAlign: "center"}}>
+      <p style={{ flex: "1 1 0%", fontSize: 40, lineHeight: "1em", textAlign: "right", color: "black", width: "80%"}}>
+        <p
+          className="full"
+          style={{
+            fontFamily: "'Love",
+            flex: "1 1 0%",
+            fontSize: 100,
+            fontWeight: "400",
+            lineHeight: "1em",
+            textAlign: "center",
+            color: "black",
+            // letterSpacing: -0.5,
+            // whiteSpace: "nowrap",
+          }}>
+          Your scribbling can be an expression
+        </p>
       </p>
     </div>
   )
